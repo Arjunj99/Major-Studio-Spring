48b85b079c9dd114f5973e95f6eba754277a23df
Adam Goren <amg1090@nyu.edu>
2020-02-25 16:10:03 -0500

    Started creating a cannon prototype

---
+++ Assets/Midterm/Scripts/New Scripts/CameraRaycast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraRaycast : MonoBehaviour
+{
+    public Transform reticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(reticle.position);
+
+        float mouseRayDist = 5.0f; // raycast distance (field of view)
+
+        RaycastHit hit = new RaycastHit();
+
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction * mouseRayDist, Color.yellow); //visualize raycast
+    }
+}
