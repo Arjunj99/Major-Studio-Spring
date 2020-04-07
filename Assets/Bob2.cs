e3690fd54636bb409e7fe7a7fb0fc9ca0a867ce2
Arjunj99 <asj397@nyu.edu>
2020-04-07 16:21:07 -0400

    Added New Lighting and Ship Materials

---
+++ Assets/Bob2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob2 : MonoBehaviour {
+
+    public float x;
+    public float y;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, x * Mathf.Sin(Time.time * y)));
+    }
+}
