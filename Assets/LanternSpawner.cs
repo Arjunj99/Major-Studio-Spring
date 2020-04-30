587818d32d846a5acf4ea25cd2e5d487cfe3a855
Arjunj99 <asj397@nyu.edu>
2020-04-30 15:46:46 -0400

    Edited Lantern Spawner

--- Assets/LanternSpawner.cs
+++ Assets/LanternSpawner.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class LanternSpawner : MonoBehaviour
 {
+    public GameObject lanternPrefab;
+    public Vector3 zone;
+    public int numOfLanterns;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        for (int i = 0; i < numOfLanterns; i++) {
+            Instantiate(lanternPrefab, new Vector3(Random.Range(-zone.x, zone.x), Random.Range(-zone.y, zone.y), Random.Range(-zone.z, zone.z)), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
