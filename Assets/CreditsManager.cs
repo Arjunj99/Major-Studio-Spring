5ec08e61783f392c58d8ae40d043d9106818655a
asj397 <asj397@nyu.edu>
2020-05-07 15:08:44 -0400

    Edited Credits Manager

--- Assets/CreditsManager.cs
+++ Assets/CreditsManager.cs
@@ -15,7 +15,7 @@ public class CreditsManager : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         text.gameObject.SetActive(false);
-        // SetUpCredits();
+        SetUpCredits();
         // RunCredits();
     }
 
