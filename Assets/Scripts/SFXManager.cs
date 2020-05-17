5ab6dd230ca856f486fb4d06fa41e0531d084fc4
Adam Goren <amg1090@nyu.edu>
2020-05-17 03:35:59 -0700

    Added sounds and new objects/prefabs, modified some terrain

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+}
